e54a3c8ae6abba0a6f03c53b1b7b1df32546608e
João (Jovi) <158322472+vianJoao@users.noreply.github.com>
2025-01-10 01:04:25 -0300

    'JIRA - 100125-0104'

--- Funcoes/MenuBandas.cs
+++ Funcoes/MenuBandas.cs
@@ -1,4 +1,5 @@
 ﻿namespace ScreenSound.Funcoes
+    
 {
     public  class MenuBandas
     {
@@ -8,7 +9,7 @@
         {
 
             Funcoes funcoes = new Funcoes();
-            funcoes.ExibirMensagemBoasVindas();
+            funcoes.ExibirLogo();
             Console.WriteLine(@"１ － Ａｄｉｃｉｏｎａｒ ｂａｎｄａ： 
 ２ － Ｌｉｓｔａｒ ｂａｎｄａｓ： 
 ３ － Ａｖａｌｉａｒ ｂａｎｄａ： 
@@ -16,12 +17,31 @@
 ０ － Ｓａｉｒ ｄｏ ｍｅｎｕ：");
 
             string opcao = Console.ReadLine();
-            if (opcao == "0")
+            if (int.TryParse(opcao,out int opcaoNumerica)) 
             {
-                Console.Clear();
-               
-                funcoes.ExibirMensagemBoasVindas();
-                MenuUsuarios.ExibirMenuInicial();
+                switch (opcaoNumerica)
+                {
+                    case 1:Console.Clear();
+                        funcoes.ExibirLogo();
+                        Console.WriteLine("Informe o nome da banda: ");
+
+                        string nomeBanda = Console.ReadLine();
+                        Console.WriteLine("Você adicionou: "+nomeBanda);
+                        break;
+                    case 2: Console.WriteLine("Aqui teremos uma função para listar todas as bandas");
+                        break;
+                    case 3: Console.WriteLine("Sera refatorado para um próximo menu");
+                       break;
+                    case 4: Console.WriteLine("Também será refatorado para um próximo menu");
+                       break;
+                    case 0:
+                       Console.Clear();
+                        funcoes.ExibirLogo();
+                        MenuUsuarios.ExibirMenuInicial();
+                        break;
+
+                }
+                
             }
             else
             {
---
+++ SQLQuery3.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (1, 'Bon Jovi', 'Crush', 12, 'Its My Life'); 
+INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (2, 'Bon Jovi', 'Crush', 1, 'Say It Isn''t So');
+INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (3, 'Bon Jovi', 'Crush', 2, 'Thank You for Loving Me');
+INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (4, 'Bon Jovi', 'Crush', 3, 'Two Story Town');
+INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (5, 'Bon Jovi', 'Crush', 4, 'Next 100 Years');
+INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (6, 'Bon Jovi', 'Crush', 5, 'Just Older');
+INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (7, 'Bon Jovi', 'Crush', 6, 'Mystery Train');
+INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (8, 'Bon Jovi', 'Crush', 7, 'Save the World');
+INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (9, 'Bon Jovi', 'Crush', 8, 'Captain Crash and the Beauty Queen From Mars');
+INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (10, 'Bon Jovi', 'Crush', 9, 'She''s a Mystery');
+INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (11, 'Bon Jovi', 'Crush', 10, 'I Got the Girl');
+INSERT INTO BANDAS (Id, Nome, Albuns, Faixas, Musica) VALUES (12, 'Bon Jovi', 'Crush', 11, 'One Wild Night');
+
+Commit;
+
+SELECT * FROM BANDAS 
+ORDER BY 1 ASC
